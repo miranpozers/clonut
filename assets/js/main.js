c4f2c8f1244dd27ed58a065f16068e82ab31dca3
miranpozers <miranpozers@gmail.com>
2024-11-13 23:23:52 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+
+function randomColor() {
+  var colors = ["#afde92", "#a9de92", "#92dea4", "#92c0de", "#92d4de"];
+  var randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
+var selectedColor = randomColor();
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 400,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: selectedColor,
+    },
+    shape: {
+      type: "polygon",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 7.891476416322726,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 148.65134865134866,
+        size_min: 1.5984015984015985,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+      distance: 500,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "bottom",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "bubble",
+      },
+      onclick: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 0.5,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 4,
+        duration: 0.3,
+        opacity: 1,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+  config_demo: {
+    hide_card: false,
+    background_color: "#b61924",
+    background_image: "",
+    background_position: "50% 50%",
+    background_repeat: "no-repeat",
+    background_size: "cover",
+  },
+});
